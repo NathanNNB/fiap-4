1e642b8d6e2404ea3d6ed57a1e322ea8e445de2b
Nathan Novais Borges <nathan-borges2011@live.com>
2025-07-26 18:32:42 -0300

    Add ML model and update prediction endpoint
    Added best_model.pkl to the models directory and updated the prediction route to accept POST requests with JSON input for close_prices. Integrated joblib and numpy for model loading and input processing. Updated requirements.txt to include necessary ML dependencies.

--- backend/app/routes/prediction.py
+++ backend/app/routes/prediction.py
@@ -1,14 +1,40 @@
+import os
 from flask import Blueprint, jsonify, request
 from flask_cors import CORS
+import numpy as np
+import joblib
 
 prediction = Blueprint("prediction", __name__)
 CORS(prediction)
 
-@prediction.route("/", methods=["GET"])
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "models", "best_model.pkl")
+model = joblib.load(MODEL_PATH)
+
+@prediction.route("/", methods=["POST"])
 def get_predicition():
 
-    param1 = request.args.get("param1", type=int)
-    param2 = request.args.get("param2", type=int)
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "JSON not found"}), 400
+    
+    close_prices = data.get("close_prices")
+    if not close_prices or not isinstance(close_prices, list):
+        return jsonify({"error": "close_prices needs to be a list with numerical values"}), 400
+    
+    if len(close_prices) < 5:
+        return jsonify({"error": "Please provide at least 5 price values for prediction"}), 400
+    
+    try:
+        # You may need to reshape the input depending on your model
+        input_array = np.array(close_prices[-60:]).reshape(1, -1)
 
-    return jsonify({"param1": param1, "param2": param2})
+        # Make the prediction
+        # prediction_value = model.predict(input_array)
+        prediction_value = "sss"
+
+
+        return jsonify({"predicted_price": float(prediction_value[0])})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
