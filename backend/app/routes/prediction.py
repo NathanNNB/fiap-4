3bb17c6be26ac9a96fd225e3cd4581ff29dad4c2
Nathan Novais Borges <nathan-borges2011@live.com>
2025-07-26 19:35:31 -0300

    Update prediction endpoint and add curl examples
    Commented out model loading and replaced prediction output with a static value for testing. Added example curl commands to README and prediction.py for easier API usage reference.

--- backend/app/routes/prediction.py
+++ backend/app/routes/prediction.py
@@ -8,7 +8,7 @@ prediction = Blueprint("prediction", __name__)
 CORS(prediction)
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "models", "best_model.pkl")
-model = joblib.load(MODEL_PATH)
+# model = joblib.load(MODEL_PATH)
 
 @prediction.route("/", methods=["POST"])
 def get_predicition():
@@ -30,7 +30,7 @@ def get_predicition():
 
         # Make the prediction
         # prediction_value = model.predict(input_array)
-        prediction_value = "sss"
+        prediction_value = [123.45]
 
 
         return jsonify({"predicted_price": float(prediction_value[0])})
@@ -38,3 +38,9 @@ def get_predicition():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+
+# http://192.168.0.32:8080/prediction/
+
+
+# curl -X POST http://192.168.0.32:8080/prediction/ -H "Content-Type: application/json" -d '{"close_prices": [130.5, 131.2, 132.0, 133.1, 134.5, 135.0, 136.2, 137.8, 138.9, 139.7, 140.5, 141.2, 142.0, 143.1, 144.3, 145.7, 146.8, 147.9, 148.5, 149.3, 150.1, 151.0, 152.4, 153.2, 154.0, 155.1, 156.3, 157.0, 158.5, 159.2, 160.0, 161.1, 162.5, 163.2, 164.0, 165.4, 166.0, 167.1, 168.3, 169.0, 170.2, 171.0, 172.5, 173.4, 174.2, 175.5, 176.0, 177.1, 178.3, 179.2, 180.0, 181.4, 182.0, 183.1, 184.3, 185.0, 186.5, 187.2, 188.8, 189.9]}'
